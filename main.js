597e8ce72e15a8cac6986f496c1d42ff3c9b1f16
Fr0styy <lassehindsberg@gmail.com>
2022-06-13 17:22:17 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", event => {
+    var accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener("click", event => {
+        accordionItemHeader.classList.toggle("active");
+
+        var accordionItemBody = accordionItemHeader.nextElementSibling;
+        if(accordionItemHeader.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        } else {
+            accordionItemBody.style.maxHeight = 0
+        }
+    });
+});
+
+})
